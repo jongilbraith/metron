592e1b2108c279ceaf7d2a406a3eb646f5b18605
Jon Gilbraith <jon@professionalnerd.com>
2008-09-16 23:33:33 +0100

    Added some tests. Not much, but it's a start.

---
+++ test/mass_test.rb
@@ -0,0 +1,36 @@
+require 'test/unit'
+require 'rubygems'
+require 'shoulda'
+require File.join(File.dirname(__FILE__), "..", "lib", "metron")
+
+class MassTest < Test::Unit::TestCase
+  
+  context "A metric mass" do
+    
+    setup do
+      @mass = MetricMass.new(100, :g)
+    end
+    
+    should "convert to mg" do
+      assert_equal @mass.to_mg, MetricMass.new(100000, :mg)
+    end
+
+    should "convert to dg" do
+      assert_equal @mass.to_dg, MetricMass.new(1000, :dg)
+    end
+
+    should "convert to g" do
+      assert_equal @mass.to_g, MetricMass.new(100, :g)
+    end
+
+    should "convert to kg" do
+      assert_equal @mass.to_kg, MetricMass.new(0.1, :kg)
+    end
+
+    should "convert to tonne" do
+      assert_equal @mass.to_tonne, MetricMass.new(0.0001, :tonne)
+    end
+    
+  end
+  
+end
