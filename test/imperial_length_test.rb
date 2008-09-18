80f4022a1f519e70f635b515f12208393db1d8bc
Jon Gilbraith <jon@professionalnerd.com>
2008-09-18 21:18:19 +0100

    Changed the conversion tests to less 'regular' numbers. So to speak.

--- test/imperial_length_test.rb
+++ test/imperial_length_test.rb
@@ -65,27 +65,27 @@ class ImperialLengthTest < Test::Unit::TestCase
     end
     
     should "convert to thou" do
-      assert_equal ImperialLength.new(1, :foot).to_thou, ImperialLength.new(12000, :thou)
+      assert_equal ImperialLength.new(2.3, :yard).to_thou, ImperialLength.new(82800, :thou)
     end
 
     should "convert to inch" do
-      assert_equal ImperialLength.new(1, :foot).to_inch, ImperialLength.new(12, :inch)
+      assert_equal ImperialLength.new(2.5, :yard).to_inch, ImperialLength.new(90, :inch)
     end
 
     should "convert to foot" do
-      assert_equal ImperialLength.new(12000, :thou).to_foot, ImperialLength.new(1, :foot)
+      assert_equal ImperialLength.new(43200, :thou).to_foot, ImperialLength.new(1.2, :yard)
     end
 
     should "convert to yard" do
-      assert_equal ImperialLength.new(3, :foot).to_yard, ImperialLength.new(1, :yard)
+      assert_equal ImperialLength.new(54, :inch).to_yard, ImperialLength.new(1.5, :yard)
     end
 
     should "convert to furlong" do
-      assert_equal ImperialLength.new(660, :foot).to_furlong, ImperialLength.new(1, :furlong)
+      assert_equal ImperialLength.new(528, :yard).to_furlong, ImperialLength.new(2.4, :furlong)
     end
 
     should "convert to mile" do
-      assert_equal ImperialLength.new(5280, :foot).to_mile, ImperialLength.new(1, :mile)
+      assert_equal ImperialLength.new(114048, :inch).to_mile, ImperialLength.new(1.8, :mile)
     end
 
 end
--- test/metric_mass_test.rb
+++ test/metric_mass_test.rb
@@ -73,23 +73,23 @@ class MetricMassTest < Test::Unit::TestCase
     end
     
     should "convert to mg" do
-      assert_equal MetricMass.new(100, :g).to_mg, MetricMass.new(100000, :mg)
+      assert_equal MetricMass.new(1.3, :kg).to_mg, MetricMass.new(1300000, :mg)
     end
 
     should "convert to dg" do
-      assert_equal MetricMass.new(100, :g).to_dg, MetricMass.new(1000, :dg)
+      assert_equal MetricMass.new(6, :kg).to_dg, MetricMass.new(60000, :dg)
     end
 
     should "convert to g" do
-      assert_equal MetricMass.new(100, :g).to_g, MetricMass.new(100, :g)
+      assert_equal MetricMass.new(34, :dg).to_g, MetricMass.new(3.4, :g)
     end
 
     should "convert to kg" do
-      assert_equal MetricMass.new(100, :g).to_kg, MetricMass.new(0.1, :kg)
+      assert_equal MetricMass.new(2860, :dg).to_kg, MetricMass.new(0.286, :kg)
     end
 
     should "convert to tonne" do
-      assert_equal MetricMass.new(100, :g).to_tonne, MetricMass.new(0.0001, :tonne)
+      assert_equal MetricMass.new(3506, :kg).to_tonne, MetricMass.new(3.506, :tonne)
     end
   
 end
